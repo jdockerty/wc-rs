eef6afd52e83eb03a4521eb9fd8f9ef755b961d9
Jack Dockerty <jdockerty19@gmail.com>
2023-10-03 21:47:58 +0100

    feat: add count bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, help = "count the number of bytes in a file")]
+    count: bool,
+
+    #[arg(short, long)]
+    file: PathBuf,
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let args = Args::parse();
+
+    match args {
+        Args { count, file } => {
+            if count {
+                let input_file = std::fs::File::open(&file)?;
+                let size_in_bytes = input_file.metadata()?.len();
+                println!("{size_in_bytes} {}", file.to_string_lossy());
+            }
+
+            Ok(())
+        }
+    }
 }
