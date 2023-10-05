86aa573518c9a4b606866b452a5f7b483bdb2197
Jack Dockerty <jdockerty19@gmail.com>
2023-10-05 20:36:34 +0100

    feat: add tests

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,43 @@ struct Args {
     file: PathBuf,
 }
 
+fn count_bytes(file_path: PathBuf) -> Result<u64, std::io::Error> {
+    let input_file = std::fs::File::open(&file_path)?;
+    let size_in_bytes = input_file.metadata()?.len();
+
+    Ok(size_in_bytes)
+}
+
 fn main() -> Result<(), std::io::Error> {
     let args = Args::parse();
 
     match args {
         Args { count, file } => {
             if count {
-                let input_file = std::fs::File::open(&file)?;
-                let size_in_bytes = input_file.metadata()?.len();
+                let size_in_bytes = count_bytes(file.to_path_buf())?;
                 println!("{size_in_bytes} {}", file.to_string_lossy());
             }
 
             Ok(())
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn count_bytes_returns_correctly() {
+        let file = NamedTempFile::new().expect("unable to create temporary file");
+        let execpted_size: u64 = 5000;
+        file.as_file()
+            .set_len(execpted_size)
+            .expect("unable to set file size");
+
+        let file_size = count_bytes(file.path().into()).expect("unable to count_bytes");
+
+        assert_eq!(file_size, execpted_size);
     }
 }
