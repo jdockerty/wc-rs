5808aecb5ce14bb7b482b3eafca4cab9e7927d60
Jack Dockerty <jdockerty19@gmail.com>
2023-10-05 21:48:22 +0100

    feat: add word count

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,17 @@ use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[arg(short, long, help = "count the number of bytes in a file")]
-    count: bool,
-
     #[arg(short, long)]
     file: PathBuf,
 
+    #[arg(short, long, help = "count the number of bytes in a file")]
+    count: bool,
+
     #[arg(short, long, help = "count the number of lines in a file")]
     lines: bool,
+
+    #[arg(short, long, help = "count the number of words in a file")]
+    words: bool,
 }
 
 fn count_bytes(file_path: PathBuf) -> Result<u64, std::io::Error> {
@@ -24,11 +27,28 @@ fn count_lines(file_path: PathBuf) -> Result<usize, std::io::Error> {
     Ok(std::fs::read_to_string(file_path)?.lines().count())
 }
 
+fn count_words(file_path: PathBuf) -> Result<usize, std::io::Error> {
+    let file = std::fs::read_to_string(file_path)?;
+    let lines = file.lines();
+
+    let mut wc = 0;
+    for line in lines {
+        wc += line.split_whitespace().count();
+    }
+
+    Ok(wc)
+}
+
 fn main() -> Result<(), std::io::Error> {
     let args = Args::parse();
 
     match args {
-        Args { count, file, lines } => {
+        Args {
+            file,
+            count,
+            words,
+            lines,
+        } => {
             if count {
                 let size_in_bytes = count_bytes(file.to_path_buf())?;
                 println!("{size_in_bytes} {}", file.to_string_lossy());
@@ -37,6 +57,11 @@ fn main() -> Result<(), std::io::Error> {
             if lines {
                 let lines = count_lines(file.to_path_buf())?;
                 println!("{lines} {}", file.to_string_lossy());
+            }
+
+            if words {
+                let word_count = count_words(file.to_path_buf())?;
+                println!("{word_count} {}", file.to_string_lossy());
             }
 
             Ok(())
@@ -67,5 +92,16 @@ mod tests {
         let lines = count_lines(file_path).expect("unable to count lines in file");
 
         assert_eq!(expected, lines);
+    }
+
+    #[test]
+    fn count_words_returns_correctly() {
+        let file_path = PathBuf::from("testdata/file.txt");
+
+        let expected: usize = 58164;
+
+        let words = count_words(file_path).expect("unable to count lines in file");
+
+        assert_eq!(expected, words);
     }
 }
